ae75a725afed8c0f3008f6f7918bc166d2ec0805
Jafar Azhar <jafar.azhar17@gmail.com>
2023-01-14 17:40:50 +0700

    hmm

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,34 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 import { Icon } from "@iconify/react";
+import Image from "next/image";
+import ava1 from "../img/ava-1.png";
+import ava2 from "../img/ava-2.png";
 
 type Props = {};
 
 export default function Skills({}: Props) {
+  const dragAreaRef = useRef(null);
+
   return (
     <div className="flex flex-col relative gap-4 h-screen md:flex-row max-w-7xl px-10 justify-center mx-auto items-center">
       <h3 className="absolute top-24 tracking-widest text-gray-500 text-2xl">
         Skills
       </h3>
 
-      <div className="flex">
-        <div>
-          <Icon
-            icon="vscode-icons:file-type-html"
-            className="w-10 h-10 md:w-20 md:h-20"
-          />
+      <div className="bg-slate-300 w-[400px] h-[400px]" ref={dragAreaRef}>
+        <div className="flex">
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            drag
+            dragConstraints={dragAreaRef}
+            className="iconBorder"
+          >
+            <Icon icon="vscode-icons:file-type-html" className="icons" />
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            drag
+            dragConstraints={dragAreaRef}
+            className="iconBorder"
+          >
+            <Icon icon="vscode-icons:file-type-css" className="icons" />
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            drag
+            dragConstraints={dragAreaRef}
+            className="iconBorder"
+          >
+            <Icon icon="logos:javascript" className="icons rounded-lg" />
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            drag
+            dragConstraints={dragAreaRef}
+            className="iconBorder"
+          >
+            <Icon icon="logos:react" className="icons" />
+          </motion.div>
         </div>
-        <div>
-          <Icon
-            icon="vscode-icons:file-type-css"
-            className="w-10 h-10 md:w-20 md:h-20"
-          />
-        </div>
-        <div>
-          <Icon icon="logos:javascript" className="w-10 h-10 md:w-20 md:h-20" />
-        </div>
-        <div>
-          <Icon icon="logos:react" className="w-10 h-10 md:w-20 md:h-20" />
-        </div>
+        <div></div>
       </div>
 
       <p className="items-center p-8 text-sm max-w-lg md:max-w-sm md:text-base xl:text-xl text-gray-500 text-justify">
