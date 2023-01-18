d825ea30a38146d933d05ce4ed9953bdf855f79c
Jafar Azhar <jafar.azhar17@gmail.com>
2023-01-19 01:43:03 +0700

    smooth scroll, hide scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Router from "next/router";
-
-Router.events.on("routeChangeStart", () => {
-  window.scrollTo(0, 0);
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
