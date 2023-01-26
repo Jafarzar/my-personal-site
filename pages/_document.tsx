3a96aa233e93001cf7604a73833ddafe568368fa
Jafar Azhar <jafar.azhar17@gmail.com>
2023-01-26 18:40:05 +0700

    fix: twitter og2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,12 +23,15 @@ export default function Document() {
         />
         <link rel="manifest" href="/site.webmanifest" />
 
-        <meta property="og:image" content="/jafarzar.jpg" />
+        <meta property="og:image" content="https://jafarzar.com/jafarzar.jpg" />
         <meta name="twitter:image:alt" content="jafarzar" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Jafarzar" />
         <meta name="twitter:description" content="My Personal Website!" />
-        <meta name="twitter:image" content="/jafarzar.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://jafarzar.com/jafarzar.jpg"
+        />
         <meta name="twitter:creator" content="@jafarzar" />
         <meta property="og:url" content="https://jafarzar.com/" />
       </Head>
